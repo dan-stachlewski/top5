9c8c41779c9521dca9df3971a18bbfd1b138f486
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-03 11:01:22 +1100

    Created a route for the Top5 website homepage: - had to make a twig file for the page - make sure the variables exist in the template so the information appears as written

--- app/src/routes.php
+++ app/src/routes.php
@@ -18,9 +18,24 @@
 
 //require 'app/src/FormsValidation.php';
 
+require 'app/src/homeRoutes.php';
 require 'app/src/placesRoutes.php';
 
+/* ============ ROUTE FOR HOMEPAGE ============ */
+/***
+ * Step 1 - add the $app->get(...);
+ * Step 2 - create the template for the render
+ * Step 3 - Makse sure the 'page_content' exists in the template like  {{ page_content|raw }}
+ */
 $app->get('/', function ($request, $response, $args) {
+
+    $flash_messages = $this->flash->getMessages();
+    $this->view->render($response, '/home/homepage_structure.twig', [
+        //'docs' => $this->toc,
+        'flash_messages' => $flash_messages,
+       // 'userLogged' => isset($_SESSION['user_id']),
+        'page_content' => "<h1>Get slim with Slim</h1>",
+    ]);
     return $response->write("HELLO top5");
 })->setName('home');
 
