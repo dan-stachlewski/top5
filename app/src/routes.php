fd0fa447baddda3a2dab24cc8c7c8e992b97c70c
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-03 12:16:49 +1100

    Updated the routes so that the homepage for Part B is ...au/client as requested by Janusz' That is the page that will have llinks - and another login for different users

--- app/src/routes.php
+++ app/src/routes.php
@@ -21,7 +21,7 @@
 require 'app/src/homeRoutes.php';
 require 'app/src/placesRoutes.php';
 
-/* ============ ROUTE FOR HOMEPAGE A ============ */
+/* ============ ROUTE FOR HOMEPAGE A LOGIN ============ */
 /***
  * Step 1 - add the $app->get(...);
  * Step 2 - create the template for the render
@@ -34,9 +34,9 @@ $app->get('/', function ($request, $response, $args) {
     $tags = $this->places->getTags();    
     
     
-    $this->view->render($response, '/home-part-a/homepage_b_structure.twig', [
+    $this->view->render($response, '/home/part-a/homepage_structure.twig', [
         //'place' => $place,
-        'tags' => $tags,//'docs' => $this->toc,
+        //'tags' => $tags,//'docs' => $this->toc,
         //'flash_messages' => $flash_messages,
        // 'userLogged' => isset($_SESSION['user_id']),
         'page_content' => "<h1>Welcome to the Top 5 website.</h1>",
@@ -44,20 +44,20 @@ $app->get('/', function ($request, $response, $args) {
 
 })->setName('homepage_a');
 
-/* ============ ROUTE FOR HOMEPAGE B ============ */
+/* ============ ROUTE FOR HOMEPAGE B LINKS TO DATA BASED ON CATEGORY ============ */
 /***
  * Step 1 - add the $app->get(...);
  * Step 2 - create the template for the render
  * Step 3 - Makse sure the 'page_content' exists in the template like  {{ page_content|raw }}
  *** NOTE *** I wanted to generate links that would take the user to each of the categories and show all for that category however Janusz said this should be for PART B All I need is a login screen for Part A
  */
-$app->get('/', function ($request, $response, $args) {
+$app->get('/client', function ($request, $response, $args) {
     
 
     $tags = $this->places->getTags();    
     
     
-    $this->view->render($response, '/home-part-b/homepage_structure.twig', [
+    $this->view->render($response, '/home/part-b/homepage_structure.twig', [
         //'place' => $place,
         'tags' => $tags,//'docs' => $this->toc,
         //'flash_messages' => $flash_messages,
