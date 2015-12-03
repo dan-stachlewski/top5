e52a5a33ce332401011e547c03d0c6b7cb55caf2
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-03 13:50:31 +1100

    Added the Login Route for the customers. Thanks to Janusz's help it is working finally!

--- app/src/customersRoutes.php
+++ app/src/customersRoutes.php
@@ -17,3 +17,45 @@
  * 
  */
 
+
+
+//NEED TO KNOW USER IS LOGGED IN AND ADMIN
+$app->map(['GET', 'POST'], '/customers/login', function ($request, $response, $args) {
+    $field_errors = [];
+    $flash_messages = $this->flash->getMessages();
+    $customer = [];
+    if ($request->isPost()) {
+        //$userService = new AuthService();
+
+        $customer['username'] = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $customer['password'] = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+
+        $customer_form = validateLoginForm($customer);
+
+        if ($customer_form['is_valid']) {
+            $valid_customer = $this->auth->authenticateUser($customer['username'], $customer['password']);
+
+            if ($valid_customer) {
+                //NEED TO KNOW USER IS LOGGED IN AND ADMIN
+                $_SESSION['customer_id'] = $valid_customer['customer_id']; //this data can now be passed throughout
+                
+                $this->flash->addMessage('success', 'Login successful');
+                return $response->withRedirect($this->router->pathFor('places-home'));
+            } else {
+                $flash_messages['danger'][] = "Incorrect Username or Email & Password combination - Please try again!";
+            }
+        } else {
+            $field_errors = $user_form['has_errors'];
+        }
+    }
+    return $this->view->render($response, 'customers/login.twig', [
+                'customer' => $customer,
+                'flash_messages' => $flash_messages,
+                'errors' => $field_errors,
+               // 'userLogged' => isset($_SESSION['user_id']),
+                'csrf' => [
+                    'name' => $request->getAttribute('csrf_name'),
+                    'value' => $request->getAttribute('csrf_value'),
+                ]
+    ]);
+})->setName('login');
