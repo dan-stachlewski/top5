bf82ce389f4ae582df3e8ed6e1e6d2dc640ee3ac
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-06 12:40:22 +1100

    Added some comments to the page that explain the code a little better.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+/* ==== index.php is now the FRONT CONTROLLER = jumping off point for all the traffic entering the site  ==== */
+/* ==== All REQUESTS & RESPONSES are DIRECTED thru the index.php file where:
+ *  they will eventually move to CONTROLLERS, 
+ * serve up VIEW code, 
+ * make updates to the dBase  
+ * index.php is the 1st step for the entire app & Slim Framework is used to achieve this
+ * ==== */
+
 // All file paths relative to root
 chdir(dirname(__DIR__));
 
@@ -9,12 +17,15 @@ session_start();
 // Simulate Customer Login *** @ the beginning so we can RETRIEVE & DISPLAY places ***
 //$_SESSION['customer_id'] = 1;
 
-
+/* ==== EXAMPLE OF [NAMESPACING] use Noodlehaus\Config to shorten the code to make it look neater   ==== */
+/* ==== [NAMESPACING] used to reduce conflics of variables and classes using the same names in the dependencies libraries   ==== */
 use Noodlehaus\Config;
+
 $configuration = Config::load('app/boot/settings.yaml');
 $settings = $configuration->get('settings', []);
 
 // Instantiate the SLIM V 3.0 FRAMEWORK
+//New instance of Slim in the $app vaiable
 $app = new \Slim\App(['settings'=>$settings]);
 
 
