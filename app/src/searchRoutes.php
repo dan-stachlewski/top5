a71c425f7b3c949e3d5498ea1f0b6b1e29ef62a2
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-07 21:28:10 +1100

    Added all the code for the search form. This setup seems to be working ok at the moment including the FormValidation.

--- app/src/searchRoutes.php
+++ app/src/searchRoutes.php
@@ -32,35 +32,67 @@
  * 
  */
 
-$app->get('/top5', function ($request, $response, $args) {
-
-    $tags = $this->places->getTags();
-
-
-    $this->view->render($response, 'search/search.twig', [
-        //'place' => $place,
-        'tags' => $tags, //'docs' => $this->toc,
-        //'flash_messages' => $flash_messages,
-        // 'userLogged' => isset($_SESSION['user_id']),
-        'page_content' => "<h1>Welcome to the Top 5 website.</h1>",
-    ]);
-})->setName('searchsss');
+//$app->get('/top5', function ($request, $response, $args) {
+//
+//    $tags = $this->places->getTags();
+//
+//
+//    $this->view->render($response, 'search/search.twig', [
+//        //'place' => $place,
+//        'tags' => $tags, //'docs' => $this->toc,
+//        //'flash_messages' => $flash_messages,
+//        // 'userLogged' => isset($_SESSION['user_id']),
+//        'page_content' => "<h1>Welcome to the Top 5 website.</h1>",
+//    ]);
+//})->setName('searchsss');
 
 
 $app->map(['GET', 'POST'], '/search', function ($request, $response, $args) {
     $search = [];
-    $search['suburb'] = 'foo';
-    $search['tag_id'] = 1;
+    //$search['suburb'] = 'foo';
+    //$search['tag_id'] = 1;
     $results = [];
-    $results = $this->places->searchPlaces($search);
+
+    $flash_messages = $this->flash->getMessages();
+    $tags = $this->places->getTags();
   
+
     if ($request->isPost()) {
+         $search['suburb'] = filter_var($_POST['search'], FILTER_SANITIZE_STRING);
+         $search['tag_id'] = filter_var($_POST['tag_id'], FILTER_SANITIZE_STRING);
          
-    }
-  //ddd($results);
-    return $this->view->render($response, 'search/results_all.twig', [
+         $search_form = validateSearchForm($search);
+
+            if ($search_form['is_valid']) {
+                $results = $this->places->searchPlaces($search);
+                $this->flash->addMessage('success', 'Search results have been found');
+                
+                
+                return $this->view->render($response, 'search/results_all.twig', [
                     'results' => $results,
-                    //'tags' => $tags,
+                    'tags' => $tags,
+                    'title' => 'Search',
+                    'flash_messages' => $flash_messages,
+//                    /* ==== THIS EFFECTS WHAT IS SHOWN ON THE CUSTOMERS LOGIN/LOGOUT DROPDOWN MENU ==== */
+//                    /* ==== REQUIRED IN EVERY ROUTE ==== */
+//                    'customerLogged' => isset($_SESSION['customer_id']),
+                    'csrf' => [
+                        'name' => $request->getAttribute('csrf_name'),
+                        'value' => $request->getAttribute('csrf_value'),
+                    ]
+                ]);    
+           
+            } else {
+                $field_errors = $places_form['has_errors'];
+            }         
+         
+         
+         
+    }//END ->isPost()
+  //ddd($results);
+    return $this->view->render($response, 'search/search.twig', [
+                    'results' => $results,
+                    'tags' => $tags,
                     'title' => 'Search',
 //                    /* ==== THIS EFFECTS WHAT IS SHOWN ON THE CUSTOMERS LOGIN/LOGOUT DROPDOWN MENU ==== */
 //                    /* ==== REQUIRED IN EVERY ROUTE ==== */
