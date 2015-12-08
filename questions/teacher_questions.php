9b081c8c01d482bde02be877ecb05f4eeb6dfef5
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-08 11:16:04 +1100

    All questions answered.

--- questions/teacher_questions.php
+++ questions/teacher_questions.php
@@ -1,16 +1,54 @@
-<?php
+ <?php
 
 /************************************************************************
  * PLACES
  ************************************************************************
  ************************************************************************
- * 3. How to display customer name so when the 1st register & login
+ * 10. 
+ * Files:
+ * FormValidation.php L58
+ * CustomerRoutes L 88, 121
+ ************************************************************************
+ ************************************************************************
+ * 9. 
+ * Files:
+ * FormValidation.php L58
+ * CustomerRoutes L 88, 121
+ ************************************************************************
+ ************************************************************************
+ * 8. [DONE] Cannot preserve the value in the search text form - FIXED WITH JANUSZ HELP
+ * Files:
+ * search.twig L9
+ * SearchrRoutes L 50-110
+ ************************************************************************
+  ************************************************************************
+ * 7. [DONE] My validation rules are not changing depending on what I type in the search field - SORTED OUT MYSELF
+ * Files:
+ * search.twig L9
+ * FormsValidation L 54-63
+ ************************************************************************
+ ************************************************************************
+ * 6. Do we need to login before we can view the results for a search?
+ * NO users who search don't need to be logged in.
+ ************************************************************************
+ * 5. [DONE] How to validate the drop down menu - DONE WORKED OUT MYSELF
+ * Files:
+ * FormValidation.php L58
+ * CustomerRoutes L 88, 121
+ ************************************************************************
+ * 4. [DONE] How to make the title dynamic for each page - DONE W JANUSZ HELP
+ * Files:
+ * layout.twig L8
+ * CustomerRoutes L 88, 121
+ * **********************************************************************
+ ************************************************************************
+ * 3. [DONE] How to display customer name so when the 1st register & login  - DONE W JANUSZ HELP
  * There is a message [No places found for No Places found for {{ customer.username }}]
  * Files:
  * places_all.twig L51
  * **********************************************************************
  ************************************************************************
- * 2. Cannot get the Customer Menu to display Logout & Dashboard for some reason
+ * 2. [DONE] Cannot get the Customer Menu to display Logout & Dashboard for some reason  - DONE W JANUSZ HELP
  * Files: 
  * CustomersRoutes.php L84-124
  * layout.twig L83-104
